f8246b02d067da5b375f8f96b14cbc53379d9032
Syed-Muhammad-Hamza2009 <hamzacamander@gmail.com>
2024-11-30 03:25:20 -0800

    Portfolio website with Custom CSS

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../app/styles/about.css';
+
+const About = () => {
+  return (
+    <div id='about' className='about-container'>
+      <h2 className='about-heading' data-aos="zoom-in-up">
+        About Me
+      </h2>
+      <p className='about-text' data-aos="zoom-in-up">
+      My name is SYED HAMZA, and I am a passionate and dedicated student GIAIC, Artifical Intelligence, Web 3.0 & Metaverse 
+      I have a deep curiosity for learning and a strong desire to acquire new skills and knowledge.
+      </p>
+    </div>
+  );
+};
+
+export default About;
